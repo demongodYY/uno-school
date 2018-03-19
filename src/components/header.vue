d6b3d66fad600da12ff455a90a7ac243cbc6a4d6
demongodYY <yyuuqqii@outlook.com>
2018-03-19 16:03:13 +0800

    init header

---
+++ src/components/header.vue
@@ -0,0 +1,42 @@
+<template>
+  <mt-header fixed class="header-container">
+    <div slot="left">
+      logo+title
+    </div>
+    <div slot="right">
+      <mt-button size="normal" plain class="button-switch">中文</mt-button>
+      <mt-button icon="more"></mt-button>
+    </div>
+  </mt-header>
+</template>
+
+<script>
+import { Header, Button } from 'mint-ui'
+export default {
+  name: 'page-header',
+  components: {
+    'mt-header': Header,
+    'mt-button': Button
+  },
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  .header-container{
+    background-color: #0e0e0e;
+    height:10%;
+  }
+  .button-switch{
+    padding: 0 1rem;
+    border: 1px solid #ffffff;
+  }
+  .mint-button{
+    margin: .5rem .5rem;
+  }
+</style>
---
+++ src/pages/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <page-header></page-header>
+</template>
+
+<script>
+import pageHeader from '../components/header'
+export default {
+  name: 'index',
+  components: {
+    'page-header': pageHeader
+  },
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
